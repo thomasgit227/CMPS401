dc71a1ee201975ffac0ea87cc385e9b9fd97d071
Thomas Floyd <tefloyd1215@gmail.com>
2021-11-11 21:51:50 -0600

    CSV Import

--- RRunPy.R
+++ RRunPy.R
@@ -1,13 +1,13 @@
 library(reticulate)
 
-# # import libraries to handle images + numpy for the math
-# #Get the VGG16 pretrained image classifier model with the imagenet weights
+#Import and Install Libraries via Python
 py_install(packages = c("numpy","pandas","tensorflow","keras","seaborn", "seaborn"))
 py_run_string('from keras.applications.vgg16 import VGG16')
 py_run_string('from tensorflow.keras.preprocessing import image')
 py_run_string('from tensorflow.keras.applications.vgg16 import preprocess_input,decode_predictions')
 py_run_string('import pandas')
-py_run_string('import matplotlib.pyplot as plt')
-py_run_string('import seaborn as sns')
+py_run_string('import csv')
 
+
+#Run Python Script for Image Recognition
 source_python("Script.py")
