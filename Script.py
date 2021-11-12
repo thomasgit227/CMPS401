92623d30d67451e93ad9030d35e8a190a83e9169
Nicholas Lajaunie <Nicholas.Lajaunie@selu.edu>
2021-11-12 17:16:52 -0600

    Take 2 on machine learning, this time got custom hamsams

--- Script.py
+++ Script.py
@@ -1,11 +1,16 @@
 # import libraries to handle images + numpy for the math
 #Get the VGG16 pretrained image classifier model with the imagenet weights
 from posixpath import dirname
-from keras.applications.vgg16 import VGG16
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.vgg16 import preprocess_input,decode_predictions
-import numpy as np
 import csv, os.path, re
+from keras.applications.mobilenet_v2 import MobileNetV2
+from keras.applications.mobilenet_v2 import preprocess_input
+from keras.applications.mobilenet_v2 import decode_predictions
+from keras.layers import Dense
+from keras import Model
+from imageio import imread
+from skimage.transform import resize
+
+import numpy as np
 
 def processImage(fileName):
   model = VGG16( weights = 'imagenet' )
@@ -103,6 +108,104 @@ def processImage(fileName):
 
         # write row for image
         csvOut.writerow(imgList)
+
+def createModel():
+
+    # Load the original model into the program
+    model = MobileNetV2(weights="imagenet")
+
+    #Create an array to hold our images for training
+    data = np.empty( (120, 224, 224, 3) )
+
+    for i in range(60):
+    
+        im = imread( 'hamster/h' + str(i+1) + ".jpg" )
+        im = preprocess_input(im)
+        im = resize( im, output_shape=(224, 224, 3 ) )
+        data[i] = im
+    
+    for i in range(60):
+    
+        print('sandwich/s' + str(i+1) + ".jpg")
+        im = imread( 'sandwich2/s' + str(i+1) + ".jpg" )
+        im = preprocess_input(im)
+        im = resize( im, output_shape=(224, 224, 3 ) )
+        data[i + 60] = im
+
+    # Label our data
+    labels = np.empty(120, dtype=int)
+    labels[:60] = 0
+    labels[60:] = 1
+
+    # Adjust our defaukt model to prepare it for retraining
+    hamsam_output = Dense(2, activation = 'softmax')
+    hamsam_output = hamsam_output(model.layers[-2].output)
+
+    hamsam_input = model.input
+    hamsam_model = Model(inputs = hamsam_input, outputs = hamsam_output)
+
+    for layer in hamsam_model.layers[:-1]:
+        layer.trainable = False
+
+    # Compile the model
+    hamsam_model.compile(
+    loss = 'sparse_categorical_crossentropy',
+    optimizer = 'adam',
+    metrics = ['accuracy']
+    )
+
+    # Train the model for 20 epochs
+    test_data = np.empty( (40, 224, 224, 3) )
+
+    for i in range(20):
+    
+        print('hamsterValid/h' + str(i+1) + ".jpg" )
+    
+        im = imread( 'hamster/h' + str(i+1) + ".jpg" )
+        im = preprocess_input(im)
+        im = resize( im, output_shape=(224, 224 ) )
+        test_data[i] = im
+    
+    for i in range(20):
+        print( 'sandwichValid/s' + str(i+1) + ".jpg"  )
+        im = imread( 'sandwich/s' + str(i+1) + ".jpg" )
+        im = preprocess_input(im)
+        im = resize( im, output_shape=(224, 224 ) )
+        test_data[i + 20 ] = im
+
+    test_labels = np.empty(40)
+    test_labels[:20] = 0
+    test_labels[20:] = 1
+
+    hamsam_model.fit(
+        x = data,
+        y = labels,
+        epochs = 20,
+        verbose = 2,
+        validation_data = (test_data, test_labels)
+    )
+
+    # Return our trained model
+    return hamsam_model
+
+
+# A function for classifying an image
+# It requires a Model to be passed in, (likely the return of createModel(),
+# and the path of an image to be classified
+
+def classify( Model, imagePath ):
+    hamsam_test = np.empty( (1, 224, 224, 3) )
+
+    im = imread( imagePath )
+    im = preprocess_input(im)
+    im = resize( im, output_shape=(224, 224, 3 ) )
+    hamsam_test[0] = im
+
+    predictions = Model.predict(hamsam_test)
+
+    return predictions
+
+
 
 imagePath = os.getcwd()
 
