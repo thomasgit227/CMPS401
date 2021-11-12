00d4ed9f9a2ed04e32e634bdee5b7ab0f65c0030
Thomas Floyd <tefloyd1215@gmail.com>
2021-11-11 21:02:07 -0600

    looks like brocolli

--- RRunPy.R
+++ RRunPy.R
@@ -1,3 +1,10 @@
 library(reticulate)
+# # import libraries to handle images + numpy for the math
+# #Get the VGG16 pretrained image classifier model with the imagenet weights
+py_install(packages = c("numpy","pandas","tensorflow","keras","pillow"))
+py_run_string('from keras.applications.vgg16 import VGG16')
+py_run_string('from tensorflow.keras.preprocessing import image')
+py_run_string('from tensorflow.keras.applications.vgg16 import preprocess_input,decode_predictions')
+py_run_string('import pandas')
 
 source_python("RunRTest.py")
