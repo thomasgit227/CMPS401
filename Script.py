0cf70d010542ad19c8ad1c004ab339f0a59f1be7
Blaine <blaine.prestenbach@selu.edu>
2021-11-11 23:01:21 -0600

    write results to csv
    Need to test running with multiple images at once

--- Script.py
+++ Script.py
@@ -1,29 +1,55 @@
+# import libraries to handle images + numpy for the math
+#Get the VGG16 pretrained image classifier model with the imagenet weights
+from keras.applications.vgg16 import VGG16
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import preprocess_input,decode_predictions
 import numpy as np
+import csv
 
-def main():
+model = VGG16( weights = 'imagenet' )
+print( model.summary() )
 
-    model = VGG16( weights = 'imagenet' )
-    print( model.summary() )
+img_path = 'h1.jpg'
 
-    img_path = 'h1.jpg'
+# load the image with color data and as a specific size then display it
+img = image.load_img(img_path, color_mode = 'rgb', target_size = (224, 224) )
+# maybe do a thing to print the current image????
 
-    # load the image with color data and as a specific size then display it
-    img = image.load_img(img_path, color_mode = 'rgb', target_size = (224, 224) )
-    # maybe do a thing to print the current image????
+# Convert the image to a 3D numpy array, 1 dimension for width, 1 for height, 1 for color
+x = image.img_to_array(img)
+x.shape
 
-    # Convert the image to a 3D numpy array, 1 dimension for width, 1 for height, 1 for color
-    x = image.img_to_array(img)
-    x.shape
+# Add a fourth dimension to the array to account for the number of images
+x = np.expand_dims(x, axis = 0 )
 
-    # Add a fourth dimension to the array to account for the number of images
-    x = np.expand_dims(x, axis = 0 )
+x = preprocess_input(x)
+features = model.predict(x)
+results = decode_predictions(features)
 
-    x = preprocess_input(x)
-    features = model.predict(x)
-    results = decode_predictions(features)
-    print(results)
-    
+print(results)
 
-#This Makes it do the Thing - Thomas from the past
-if __name__ == "__main__":
-    main();
+with open("results.csv", "w") as csvFile:
+  csvOut = csv.writer(csvFile)
+  csvOut.writerow(["Hamster", "Sandwich"])
+
+  for currImage in results:
+    imgList = []
+
+    # append 'hamster' probability or 0
+    for tup in currImage:
+      if "hamster" in tup[1]:
+        imgList.append(tup[2])
+
+    if len(imgList) == 0:
+      imgList.append(0.0)
+
+    # append 'sandwich' probability or 0
+    for tup in currImage:
+      if "sandwich" in tup[1]:
+        imgList.append(tup[2])
+
+    if len(imgList) == 1:
+      imgList.append(0.0)
+
+    # write row for image
+    csvOut.writerow(imgList)
